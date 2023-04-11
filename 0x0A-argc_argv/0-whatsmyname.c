261700f50ebf8287ed8cf7feae76af22e0faf966
CoenraadCrause183 <ravenger183@gmail.com>
2023-04-11 13:13:28 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints the name
+ *
+ * @argc: arguments nr
+ *
+ * @argv: arguments array
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
