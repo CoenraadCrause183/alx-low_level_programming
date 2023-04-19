6bcd7194cb9da802cd97b96e60738dc729c2ad45
CoenraadCrause183 <ravenger183@gmail.com>
2023-04-18 19:17:08 -0700

    update

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a dog
+ *
+ * @d: dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nul)";
+
+	if (d->owner == NULL)
+		d->owner = "(nul)";
+
+	printf("Name: %s\nAge: %f\nOwner %s\n", d->name, d->age, d->owner);
+}
