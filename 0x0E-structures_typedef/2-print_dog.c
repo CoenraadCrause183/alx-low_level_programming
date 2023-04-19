7d6280c4af50cc24860111b07e2172d0d6061c5f
CoenraadCrause183 <ravenger183@gmail.com>
2023-04-18 19:18:53 -0700

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,10 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		d->name = "(nul)";
+		d->name = "(nil)";
 
 	if (d->owner == NULL)
-		d->owner = "(nul)";
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner %s\n", d->name, d->age, d->owner);
 }
