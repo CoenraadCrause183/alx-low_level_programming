2b0d75c723b5f9f4794af7ef0a4184914917b974
CoenraadCrause183 <ravenger183@gmail.com>
2023-04-28 20:18:50 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists"
+
+/**
+ * first - prints before main
+ *
+ * Return: always 0
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
