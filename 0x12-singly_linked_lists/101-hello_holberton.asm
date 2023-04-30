27b9a6f31dfdb33a51595967b963be5626ffd154
CoenraadCrause183 <ravenger183@gmail.com>
2023-04-29 18:09:07 -0700

    hello

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,11 @@
+global	main
+extern	printf
+
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+/* asked a friend from uni for help and some yt vids */
+format: db `Hello, Holberton\n`,0
