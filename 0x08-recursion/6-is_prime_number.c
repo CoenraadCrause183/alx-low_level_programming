d6d8276b7c68f0561c3fc91059658e40039a8c54
CoenraadCrause183 <ravenger183@gmail.com>
2023-05-01 16:45:21 -0700

    prime nr

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if prime nr
+ *
+ * @n: nr to check
+ *
+ * Return: always 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - check if prime
+ *
+ * @n: nr to check
+ *
+ * @i: iterator
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 %% i > 0)
+		return (0);
+
+	return (actual_prime(n, i - 1));
+/* CC */
+}
