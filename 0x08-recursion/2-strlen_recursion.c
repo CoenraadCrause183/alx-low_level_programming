5d8a5076401d05874498c5b1698f391321be9494
CoenraadCrause183 <ravenger183@gmail.com>
2023-05-01 15:51:58 -0700

    str recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of str
+ *
+ * @s: string
+ *
+ * Return: always 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	int longint = 0;
+
+	if (*s)
+	{
+		longint++;
+		longint += _strlen_recursion(s + 1);
+	}
+
+	return (longint);
+}
