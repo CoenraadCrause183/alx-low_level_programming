57d58425637bc0e1c025303b7b9f10fe8c7c8e7b
CoenraadCrause183 <ravenger183@gmail.com>
2023-05-01 16:27:29 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - square root
+ *
+ * @n: nr to be rooted
+ *
+ * Return: always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	
+	return (actual_sqrt_recursion(n, 0));
+}
+
+/** actual_sqrt_recursion - natural square root
+ *
+ * @n: nr to be rooted
+ *
+ * @i: the iterator
+ *
+ * Return: always 0
+ */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+
+	return (actual_sqrt_recursion(n, i + 1));
+/**
+ * CC
+ */
+}
