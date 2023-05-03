9e979d2447d38e3a74437a37364973930f69feb4
CoenraadCrause183 <ravenger183@gmail.com>
2023-05-03 15:19:26 -0700

    sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all list data
+ *
+ * @head: first node
+ *
+ * Return: always 0
+ */
+
+int sum_listint(listint_t *head)
+{
+	int i = 0;
+	listint_t *lst;
+
+	while (lst)
+	{
+		i += lst->n;
+		lst = lst->next;
+	}
+
+	return (i);
+}
