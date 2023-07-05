49958ec6bcb3768767972c7f3b44fbecf4d537df
CoenraadCrause183 <ravenger183@gmail.com>
2023-07-04 18:33:46 -0700

    update

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * argstostr - main
+ *
+ * @ac: input
+ *
+ * @av: array
+ *
+ * Return: always 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i1, i2, i3 = 0, i4 = 0;
+	char *c1;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i1 = 0; i1 < ac; i1++)
+	{
+		for (n = 0; av[i1][i2]; i2++)
+			i4++;
+	}
+
+	i4 += ac;
+
+	c1 = malloc(sizeof(char) * i4 + 1);
+
+	if (c1 == NULL)
+		return (NULL);
+
+	for (i1 = 0; i1 < ac; i1++)
+	{
+		for (i2 = 0; av[i1][i2]; i2++)
+		{
+			c1[i3] = av[i1][i2];
+			i3++;
+		}
+		if (c1[i3] == '\0')
+		{
+			c1[i3++] = '\n';
+		}
+	}
+/*CC*/
+	return (c1);
+}
