7b21b200bcbaa2819147b244dad62d2fec601e6e
CoenraadCrause183 <ravenger183@gmail.com>
2023-07-04 19:00:17 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i1 < index - 1)
 	{
-		if (!lsttmp || !(lsttmp->next))
+		if (!lsttmp1 || !(lsttmp1->next))
 			return (-1);
 		lsttmp1 = lsttmp1->next;
 		i1++;
